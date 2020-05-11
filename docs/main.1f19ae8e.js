812e6cc50df85cfd49c9908d7fd01448af807ac3
evabrinkmann <brinkmann.eva@web.de>
2020-05-11 13:38:15 +0200

    Add, toggle and animate burger menu

--- docs/main.1f19ae8e.js
+++ docs/main.1f19ae8e.js
@@ -118,7 +118,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
-
+$(document).ready(function () {
+  $('.menu-toggler').on('click', function () {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  });
+});
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
