23598b4f29d2fc8b034dbe695dd14218d6874ade
evabrinkmann <brinkmann.eva@web.de>
2020-05-12 15:33:12 +0200

    Add media query to landing text and navigation

--- docs/main.af46ece4.js
+++ docs/main.af46ece4.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./img/sitebgblue.jpg":[["sitebgblue.1d5f63a9.jpg","img/sitebgblue.jpg"],"img/sitebgblue.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./img/sitebgblue.jpg":[["sitebgblue.1d5f63a9.jpg","img/sitebgblue.jpg"],"img/sitebgblue.jpg"],"./img/sitebg2blue.jpg":[["sitebg2blue.b4b35de3.jpg","img/sitebg2blue.jpg"],"img/sitebg2blue.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
